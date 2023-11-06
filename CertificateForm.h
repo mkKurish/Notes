db1725691f4b2763ba93621bdfbf7e1184996fdd
mkKurish <kurish04@mail.ru>
2023-11-06 15:16:04 +0300

    Corrected FaQ

--- CertificateForm.h
+++ CertificateForm.h
@@ -1,11 +1,11 @@
 #pragma once
+#include <math.h>
 
 namespace Notes {
 
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
-	using System::Collections::Generic::List;
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
@@ -36,7 +36,8 @@ namespace Notes {
 			}
 		}
 	private: System::Windows::Forms::Button^ returnBtn;
-	private: System::Windows::Forms::ListBox^ faqListBox;
+	private: System::Windows::Forms::RichTextBox^ faqListBox;
+
 
 	private:
 		/// <summary>
@@ -53,7 +54,7 @@ namespace Notes {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(CertificateForm::typeid));
 			this->returnBtn = (gcnew System::Windows::Forms::Button());
-			this->faqListBox = (gcnew System::Windows::Forms::ListBox());
+			this->faqListBox = (gcnew System::Windows::Forms::RichTextBox());
 			this->SuspendLayout();
 			// 
 			// returnBtn
@@ -75,15 +76,14 @@ namespace Notes {
 			this->faqListBox->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->faqListBox->FormattingEnabled = true;
-			this->faqListBox->ItemHeight = 20;
-			this->faqListBox->Location = System::Drawing::Point(12, 5);
+			this->faqListBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->faqListBox->Location = System::Drawing::Point(13, 12);
 			this->faqListBox->Name = L"faqListBox";
-			this->faqListBox->Size = System::Drawing::Size(454, 344);
+			this->faqListBox->ReadOnly = true;
+			this->faqListBox->Size = System::Drawing::Size(453, 357);
 			this->faqListBox->TabIndex = 1;
-			this->faqListBox->DrawItem += gcnew System::Windows::Forms::DrawItemEventHandler(this, &CertificateForm::faqListBox_DrawItem);
-			this->faqListBox->MeasureItem += gcnew System::Windows::Forms::MeasureItemEventHandler(this, &CertificateForm::faqListBox_MeasureItem);
-			this->faqListBox->Resize += gcnew System::EventHandler(this, &CertificateForm::faqListBox_Resize);
+			this->faqListBox->Text = resources->GetString(L"faqListBox.Text");
 			// 
 			// CertificateForm
 			// 
@@ -92,130 +92,19 @@ namespace Notes {
 			this->ClientSize = System::Drawing::Size(478, 444);
 			this->Controls->Add(this->faqListBox);
 			this->Controls->Add(this->returnBtn);
-			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->MinimumSize = System::Drawing::Size(500, 500);
 			this->Name = L"CertificateForm";
 			this->Text = L"Справка";
-			this->Load += gcnew System::EventHandler(this, &CertificateForm::CertificateForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	// A variable indicating the type of the text title.
-	private: String^ CHAPTER_TYPE = gcnew String("chapter");
-	// A variable indicating the type of the text topic.
-	private: String^ TOPIC_TYPE = gcnew String("topic");
-	// A variable indicating the type of the text basic content.
-	private: String^ BODY_TYPE = gcnew String("body");
-
-	/*
-	Function called when loading the CertificationForm.
-	*/
-	private: System::Void CertificateForm_Load(System::Object^ sender, System::EventArgs^ e) {
-
-		// String#1 - type (chapter, topic, body); String#2 - content.
-		List<Tuple<String^, String^>^>^ faqData = gcnew List<Tuple<String^, String^>^>();
-
-		faqData->Add(gcnew Tuple<String^, String^>(CHAPTER_TYPE, gcnew String("Программа \"Заметки\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Эта программа создана для того, чтобы ....\nВы сможете...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(CHAPTER_TYPE, gcnew String("Главный экран")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(CHAPTER_TYPE, gcnew String("Экран создания заметки")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-		faqData->Add(gcnew Tuple<String^, String^>(TOPIC_TYPE, gcnew String("Кнопка \"Добавить\"")));
-		faqData->Add(gcnew Tuple<String^, String^>(BODY_TYPE, gcnew String("-делает .. способом ..\n-можно воспользоваться для ...")));
-
-
-		faqListBox->DataSource = faqData;
-
-		faqListBox->DrawMode = DrawMode::OwnerDrawVariable;
-	}
-
-	/*
-	Function called when drawing an object of the faqLisBox list.
-	*/
-	private: System::Void faqListBox_DrawItem(System::Object^ sender, System::Windows::Forms::DrawItemEventArgs^ e) {
-		if ((e->State & DrawItemState::Selected) == DrawItemState::Selected)
-			e = gcnew DrawItemEventArgs(e->Graphics, e->Font, e->Bounds,
-				e->Index, e->State ^ DrawItemState::Selected, e->ForeColor, SystemColors::Window);
-		Brush^ brush = Brushes::Black;
-		// String#1 - type (chapter, topic, body); String#2 - content.
-		Tuple<String^, String^>^ currentItem = (Tuple<String^, String^>^) faqListBox->Items[e->Index];
-		Drawing::StringFormat^ format = gcnew Drawing::StringFormat();
-		Drawing::Font^ font;
-		if (currentItem->Item1 == CHAPTER_TYPE) {
-			font = gcnew Drawing::Font("Microsoft Sans Serif", 16, FontStyle::Bold);
-			format->Alignment = StringAlignment::Center;
-		}
-		else if (currentItem->Item1 == TOPIC_TYPE) {
-			font = gcnew Drawing::Font("Microsoft Sans Serif", 13, FontStyle::Bold);
-		}
-		else {
-			font = gcnew Drawing::Font("Microsoft Sans Serif", 10, FontStyle::Regular);
-		}
-		Drawing::SizeF^ size = e->Graphics->MeasureString(currentItem->Item2, font);
-		Drawing::RectangleF^ rect = gcnew Drawing::RectangleF(e->Bounds.Left, e->Bounds.Top, faqListBox->Width - 15, size->Height);
-		e->DrawBackground();
-		e->Graphics->DrawString(currentItem->Item2, font, brush, *rect, format);
-	}
-
-	/*
-	Function called when measuring an object of the faqListBox list.
-	*/
-	private: System::Void faqListBox_MeasureItem(System::Object^ sender, System::Windows::Forms::MeasureItemEventArgs^ e) {
-		if (faqListBox->Items->Count <= 0)
-			return;
-		// String#1 - type (chapter, topic, body); String#2 - content.
-		Tuple<String^, String^>^ currentItem = dynamic_cast<Tuple<String^, String^>^>(faqListBox->Items[e->Index]);
-		Drawing::Font^ font;
-		if (currentItem->Item1 == CHAPTER_TYPE)
-			font = gcnew Drawing::Font("Microsoft Sans Serif", 16, FontStyle::Bold);
-		else if (currentItem->Item1 == TOPIC_TYPE)
-			font = gcnew Drawing::Font("Microsoft Sans Serif", 13, FontStyle::Bold);
-		else
-			font = gcnew Drawing::Font("Microsoft Sans Serif", 10, FontStyle::Regular);
-		Drawing::SizeF^ size = e->Graphics->MeasureString(currentItem->Item2, font);
-		e->ItemHeight = size->Height;
-	}
-
-	/*
-	Function called when resizing the faqListBox.
-	*/
-	private: System::Void faqListBox_Resize(System::Object^ sender, System::EventArgs^ e) {
-		faqListBox->Invalidate();
-	}
-
 	/*
 	Function called when the resetBtn button is pressed.
 	*/
 	private: System::Void returnBtn_Click(System::Object^ sender, System::EventArgs^ e) {
 		Close();
 	}
-	};
+};
 }
